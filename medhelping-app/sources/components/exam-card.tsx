8146ac04cd0142d2ddede141d82467771f86b4c4
pedrovonkarma <heeypedroo@gmail.com>
2023-06-24 02:34:51 -0300

    more advances

--- medhelping-app/sources/components/exam-card.tsx
+++ medhelping-app/sources/components/exam-card.tsx
@@ -2,10 +2,12 @@ import { useNavigation } from "expo-router";
 import { ImageBackground, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 interface ExamCardProps {
-    category: string;
+    categories: string[];
     exam: string;
     name: string;
     date: string;
+    image: string;
+
 }
 const styles = StyleSheet.create({
     imageBackground: {
@@ -21,11 +23,11 @@ export default function ExamCard(props: ExamCardProps) {
     function handleClick(){
         navigation.navigate("viewPublication", {id: 1})
     }
-    const { category, exam, name, date } = props;
+    const { categories, exam, name, date, image } = props;
     return (<>
         <TouchableOpacity onPress={() => handleClick()} activeOpacity={0.8} style={{width: '45%'}} className="mb-6 aspect-square">
-            <ImageBackground style={styles.imageBackground} className="w-full relative cover items-center justify-center" source={backgroundImage}>
-                <Text className="font-500 absolute top-1 left-1 text-xs px-3 pt-1 text-white rounded-full bg-[#000000cc]">{category}</Text>
+            <ImageBackground style={styles.imageBackground} className="w-full relative cover items-center justify-center" source={image? image : backgroundImage}>
+                <Text className="font-500 absolute top-1 left-1 text-xs px-3 pt-1 text-white rounded-full bg-[#000000cc]">{categories.map((i) => i+' ')}</Text>
                 <Text className="font-900 pb-2 pt-4 px-2 text-base text-center text-white">{exam}</Text>
                 <Text className="font-400 text-center text-xs text-white">{name}, em {date}</Text>
             </ImageBackground>
