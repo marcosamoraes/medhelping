8a895091905d4c33dcb97db7e98891ab7be7a0e4
Marcos Moraes <contato@marcosamoraes.com>
2023-07-11 17:56:15 -0300

    tracking transparency

--- medhelping-app/app/index.tsx
+++ medhelping-app/app/index.tsx
@@ -4,9 +4,20 @@ import SplashScreen from '../sources/screens/splash_screen_screen';
 import { useFonts, Catamaran_300Light, Catamaran_400Regular, Catamaran_500Medium, Catamaran_600SemiBold, Catamaran_700Bold, Catamaran_800ExtraBold, Catamaran_900Black } from '@expo-google-fonts/catamaran';
 import Routes from '@routes/index';
 import { View } from 'react-native';
+import { useEffect } from 'react';
+import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({ Catamaran_300Light, Catamaran_400Regular, Catamaran_500Medium, Catamaran_600SemiBold, Catamaran_700Bold, Catamaran_800ExtraBold, Catamaran_900Black })
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestTrackingPermissionsAsync();
+      if (status === 'granted') {
+        console.log('Yay! I have user permission to track data');
+      }
+    })();
+  }, []);
 
   return (
     <AuthProvider>
