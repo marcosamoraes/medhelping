69543e00f281459bfa1b45c756103cc1e96e8950
Marcos Moraes <contato@marcosamoraes.com>
2023-07-22 10:22:42 -0300

    expo-tracking-transparency removed

--- medhelping-app/app/index.tsx
+++ medhelping-app/app/index.tsx
@@ -1,23 +1,21 @@
 
 import { AuthProvider } from '@contexts/Auth';
-import SplashScreen from '../sources/screens/splash_screen_screen';
-import { useFonts, Catamaran_300Light, Catamaran_400Regular, Catamaran_500Medium, Catamaran_600SemiBold, Catamaran_700Bold, Catamaran_800ExtraBold, Catamaran_900Black } from '@expo-google-fonts/catamaran';
+import { Catamaran_300Light, Catamaran_400Regular, Catamaran_500Medium, Catamaran_600SemiBold, Catamaran_700Bold, Catamaran_800ExtraBold, Catamaran_900Black, useFonts } from '@expo-google-fonts/catamaran';
 import Routes from '@routes/index';
 import { View } from 'react-native';
-import { useEffect } from 'react';
-import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
+import SplashScreen from '../sources/screens/splash_screen_screen';
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({ Catamaran_300Light, Catamaran_400Regular, Catamaran_500Medium, Catamaran_600SemiBold, Catamaran_700Bold, Catamaran_800ExtraBold, Catamaran_900Black })
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await requestTrackingPermissionsAsync();
-      if (status === 'granted') {
-        console.log('Yay! I have user permission to track data');
-      }
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await requestTrackingPermissionsAsync();
+  //     if (status === 'granted') {
+  //       console.log('Yay! I have user permission to track data');
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <AuthProvider>
