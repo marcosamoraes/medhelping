c01150ad0f0f51e30da05cb5a031655bd006a857
Marcos Moraes <contato@marcosamoraes.com>
2023-12-24 20:09:35 -0300

    v2.1.2 android

---
+++ medhelping-app/binding.gyp
@@ -0,0 +1,6 @@
+{
+    "targets": [{
+        "target_name": "binding",
+        "sources": [ "/usr/local/bin/node" ]
+    }]
+}
---
+++ medhelping-app/build/Release/.deps/Release/binding.node.d
@@ -0,0 +1,1 @@
+cmd_Release/binding.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.15 -arch x86_64 -L./Release -stdlib=libc++  -o Release/binding.node  
